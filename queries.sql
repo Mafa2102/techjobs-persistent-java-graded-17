8c809837d136ad8e6e6d3f102e66c5a4ead0508d
Maria Usina <maryusina@Mac.attlocal.net>
2025-01-27 19:32:27 -0600

    First attempt

--- queries.sql
+++ queries.sql
@@ -1,7 +1,24 @@
 --Part 1
+SELECT * FROM techjobs.job;
+id int(11) PRIMARY KEY
+employer varchar(255) NOT NULL
+name varchar(255) NOT NULL
+skills varchar(255) NOT NULL
 
 --Part 2
 
+ SELECT name
+ FROM employer
+ WHERE location = "St. Louis City";
+
 --Part 3
 
+ DROP TABLE job;
+
 --Part 4
+
+SELECT *
+FROM skill
+LEFT JOIN job_skills ON job_skills.skills_id = skill.id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,47 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotNull(message = "Location must not be blank")
+    @Size(max = 100, message = "Location must be under 100 characters.")
+    private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer(){} //allows Hibernate to create an object
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation( String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public Employer(String location) {
+        super();
+        this.location = location;
+    }
 }
