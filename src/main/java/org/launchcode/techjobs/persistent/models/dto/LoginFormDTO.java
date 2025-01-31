e10902eaf321d29bfaab61530616cefd11d4cb75
Maria Usina <maryusina@Mac.attlocal.net>
2025-01-30 20:35:42 -0600

    Auth

--- src/main/java/org/launchcode/techjobs/persistent/models/dto/LoginFormDTO.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/dto/LoginFormDTO.java
@@ -16,6 +16,10 @@ public class LoginFormDTO {
     @Size(min = 5, max = 30, message = "Invalid password. Must be between 5 and 30 characters.")
     private String password;
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    private String name;
+
     public String getUsername() {
         return username;
     }
@@ -32,4 +36,11 @@ public class LoginFormDTO {
         this.password = password;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/dto/RegisterFormDTO.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/dto/RegisterFormDTO.java
@@ -4,6 +4,8 @@ public class RegisterFormDTO extends LoginFormDTO {
 
     private String verifyPassword;
 
+    private String name;
+
     public String getVerifyPassword() {
         return verifyPassword;
     }
@@ -12,4 +14,11 @@ public class RegisterFormDTO extends LoginFormDTO {
         this.verifyPassword = verifyPassword;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
